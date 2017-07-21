278c3b557aa714d8b87506f0f4a0756cb6030b64
Komica20160704 <bluecrazybear@gmail.com>
2017-07-22 00:28:19 +0800

    my first sinatra

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+# http://example.com/
+get '/' do
+  'Hellow world!'
+end
+
+# http://example.com/time
+get '/time' do
+  # This will run views/time.erb then return
+  erb :time
+end
